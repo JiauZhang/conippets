89d7f97cef75bc9f67dffcbd30a9b56850e063c8
JiauZhang <1743960454@qq.com>
2023-09-20 20:44:22 +0800

    improve

--- conippets/time.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-def local_time():
-    utc_time = datetime.datetime.utcnow()
-    time_delta = datetime.timedelta(hours=8)
-    my_time = utc_time + time_delta
-    return my_time
