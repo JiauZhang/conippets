36690f161d8350cf0261650476e6984d8745389c
JiauZhang <1743960454@qq.com>
2025-04-26 12:16:41 +0800

    add rmdir

--- conippets/path.py
+++ conippets/path.py
@@ -1,8 +1,11 @@
-import os
+import os, shutil
 
-def rm(path, missing_ok=True):
+def rm(path, missing_ok=False):
     try:
         os.remove(path)
     except FileNotFoundError:
         if not missing_ok:
             raise
+
+def rmdir(dir, missing_ok=False):
+    shutil.rmtree(dir, ignore_errors=missing_ok)
